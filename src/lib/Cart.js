b4a185bc1008934e73af7550a570e2b857afa971
miguel marcondes filho <miguelmarcondesfilho@gmail.com>
2020-11-11 15:44:31 -0300

    Simplifying total value calc,taxes calc and add goods methods

--- src/lib/Cart.js
+++ src/lib/Cart.js
@@ -1,41 +1,22 @@
 import Product from './Product'
 
-const SALES_TAX_EXEMPT = ['book', 'food', 'medical', 'chocolate', 'pill'],
-  NAME_REGEX = /(?<=[0-9] )(.*)(?= at )/
-
 export default class Cart {
   productsList = []
-  salesTaxes = 0.0
-  total = 0.0
 
   addGoods(items_list) {
-    items_list.forEach((item) => {
-      const splittedItem = item.split(' ')
-
-      let attributes = {
-        name: new RegExp(NAME_REGEX).exec(item)[0],
-        quantity: parseInt(splittedItem[0]),
-        price: parseFloat(splittedItem[splittedItem.length - 1]),
-        exempt: new RegExp(SALES_TAX_EXEMPT.join('|')).test(item),
-        imported: item.includes('imported')
-      }
-      let product = new Product(attributes)
-
-      this.updateSalesTaxes(product.taxes, product.quantity)
-      this.updateTotal(product.total)
-
-      this.productsList.push(product)
-    })
+    items_list.forEach((item) => this.productsList.push(new Product(item)))
   }
 
-  updateSalesTaxes(item_taxes, item_quantity) {
-    this.salesTaxes =
-      parseFloat(this.salesTaxes) + parseFloat(item_taxes * item_quantity)
-    this.salesTaxes = this.salesTaxes.toFixed(2)
+  calcSalesTaxesTotal() {
+    return this.productsList.reduce((acc, product) => {
+      return acc + (product.quantity * product.taxes)
+    }, 0.0).toFixed(2)
   }
 
-  updateTotal(item_total) {
-    this.total += parseFloat(item_total.toFixed(2))
+  calcTotalValue() {
+    return this.productsList.reduce((acc, product) => {
+      return acc + product.total
+    }, 0.0).toFixed(2)
   }
 
   formattedSummary() {
@@ -45,8 +26,8 @@ export default class Cart {
 
     return [
       ...products,
-      `Sales Taxes: ${this.salesTaxes}`,
-      `Total: ${this.total}`
+      `Sales Taxes: ${this.calcSalesTaxesTotal()}`,
+      `Total: ${this.calcTotalValue()}`
     ]
   }
 }
